1a89bcfc3fd8be20010c52c5b6854751f9391662
Anolla <annehaddad96@yahoo.com>
2021-07-02 12:34:43 +0300

    files structure

---
+++ src/context/socket.js
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+import io from 'socket.io-client';
+  export const SocketContext = createContext();
+
+function SocketProvider(props) {
+
+    const options = {
+        transports: ['websocket'],
+      };
+      const socket = io('localhost:3030/', options);
+   
+    const state = {
+    socket
+    };
+    return (
+      <SocketContext.Provider value={state}>
+        {props.children}
+      </SocketContext.Provider>
+    );
+  }
+  export default SocketProvider;
