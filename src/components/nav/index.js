e3cd3daf753c806b15af1cce1218e0b3d358f8a6
AbdulhakimZatar <hakimzatar@gmail.com>
2021-07-03 23:11:45 +0300

    header

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,11 +1,16 @@
 import { useContext } from "react";
 import { DataContext } from "../../context/data";
+import { Heading } from '@chakra-ui/react'
 import './style.scss'
+import logo from '../../utilities/assets/logo.svg'
 
 function Nav() {
   const context = useContext(DataContext);
   // logo center
-  return <header></header>;
+  return <header className='flexRow'>
+    <img style={{ width: '42px' }} alt='logo' src={logo} />
+    <Heading ml='12px' as="h1" color='#F5F5F5'  size="lg" >Insighteer</Heading>
+  </header>;
 }
 
 export default Nav;
