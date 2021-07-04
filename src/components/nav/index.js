fc09c540c5b6af9ac2fa89baf132e35e6d87fe9a
AbdulhakimZatar <hakimzatar@gmail.com>
2021-07-04 07:21:22 +0300

    header logo font size

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -8,8 +8,8 @@ function Nav() {
   const context = useContext(DataContext);
   // logo center
   return <header className='flexRow'>
-    <img style={{ width: '42px' }} alt='logo' src={logo} />
-    <Heading ml='12px' as="h1" color='#F5F5F5'  size="lg" >Insighteer</Heading>
+    <img style={{ width: '38px' }} alt='logo' src={logo} />
+    <Heading ml='12px' as="h1" color='#F5F5F5'  fontSize='24px' >Insighteer</Heading>
   </header>;
 }
 
